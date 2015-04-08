b6b84b80c8014f7d914b2f74b0bf5e747d9f75c8
Leo <xd1936@users.noreply.github.com>
2015-04-08 13:09:29 -0800

    Trying out moment-based minute hand calc

--- clockwork.js
+++ clockwork.js
@@ -165,7 +165,7 @@ function setbkg(){
 delayMinuteHand = null;
 
 function updateMinuteHand() {
-    var d = new Date();
+    var d = moment();
     var m = d.getMinutes();
     var s = d.getSeconds();
     
