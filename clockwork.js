cbbd2c812419e34a2111ceee0c09b9e8cd494343
Leo Herzog <xd1936@gmail.com>
2017-09-14 09:33:38 -0400

    Clean up and update

--- clockwork.js
+++ clockwork.js
@@ -1,73 +1,73 @@
 function getWindowSize() {
     var myWidth;
     var myHeight;
-    
-    if( typeof( window.innerWidth ) == 'number' ) {
+
+    if (typeof (window.innerWidth) == 'number') {
         //Non-IE
         myWidth = window.innerWidth;
         myHeight = window.innerHeight;
-    } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+    } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
         //IE 6+ in 'standards compliant mode'
         myWidth = document.documentElement.clientWidth;
         myHeight = document.documentElement.clientHeight;
-    } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+    } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
         //IE 4 compatible
         myWidth = document.body.clientWidth;
         myHeight = document.body.clientHeight;
     }
-    
-    return [myWidth,myHeight];
+
+    return [myWidth, myHeight];
 }
 
 waitForIt = null;
 
-function setbkg(){
-    
+function setbkg() {
+
     /* ====================================================================
     http://www.npr.org/audiohelp/progstream.html
     ==================================================================== */
-    
+
     var paddingTop = 380; // from the TOP of the IMAGE to the BORDER of the CLOCK
     var outerHand = 20; // from the TOP of the 'outer' HAND to the BORDER of the CLOCK
     var fullHandSize = 1000; // from the TOP of the 'outer' HAND to the CENTER of the CLOCK
-    
+
     // schedule, starting with Sunday, ex. [[pic.png,#ofdays],[pic.png,#ofdays]],
     var program = [
-    [['other2014.png',2],['me2014.png',5]],
-    [['other2014.png',2],['me2014.png',5]],
-    [['other2014.png',2],['atc2014.png',5]],
-    [['other2014.png',2],['atc2014.png',5]],
-    [['other2014.png',7]],
-    [['other2014.png',7]],
-    [['other2014.png',2],['atc2014.png',5]],
-    [['other2014.png',2],['atc2014.png',5]],
-    [['other2014.png',7]],
-    [['other2014.png',7]],
-    [['other2014.png',7]],
-    [['other2014.png',7]],
-    [['other2014.png',1],['me2014.png',5],['other2014.png',1]],
-    [['other2014.png',1],['me2014.png',5],['other2014.png',1]],
-    [['other2014.png',7]],
-    [['other2014.png',7]],
-    [['other2014.png',7]],
-    [['other2014.png',7]],
-    [['other2014.png',7]],
-    [['other2014.png',7]],
-    [['other2014.png',7]],
-    [['other2014.png',7]],
-    [['other2014.png',1],['atc2014.png',5],['other2014.png',1]],
-    [['other2014.png',1],['atc2014.png',5],['other2014.png',1]],
+        [['other2014.png',2],['me2014.png',5]],
+        [['other2014.png',2],['me2014.png',5]],
+        [['other2014.png',2],['atc2014.png',5]],
+        [['other2014.png',2],['atc2014.png',5]],
+        [['other2014.png',7]],
+        [['other2014.png',7]],
+        [['other2014.png',2],['atc2014.png',5]],
+        [['other2014.png',2],['atc2014.png',5]],
+        [['other2014.png',7]],
+        [['other2014.png',7]],
+        [['other2014.png',7]],
+        [['other2014.png',7]],
+        [['other2014.png',1],['me2014.png',5],['other2014.png',1]],
+        [['other2014.png',1],['me2014.png',5],['other2014.png',1]],
+        [['other2014.png',7]],
+        [['other2014.png',7]],
+        [['other2014.png',7]],
+        [['other2014.png',7]],
+        [['other2014.png',7]],
+        [['other2014.png',7]],
+        [['other2014.png',7]],
+        [['other2014.png',7]],
+        [['other2014.png',1],['atc2014.png',5],['other2014.png',1]],
+        [['other2014.png',1],['atc2014.png',5],['other2014.png',1]],
     ];
-    
+
     // getting the array for this hour
     var h = moment().tz('America/New_York').get('hour');
     var thisHour = program[h];
-    
+
     var img = null;
-    
+
     // if this hour is not yet configured
     if (!thisHour) {
-        console.error('The hour '+h+' is not configured yet.');
+        console.error('The hour ' + h + ' is not configured yet.');
     } else {
         // if it is
         var dayImg = [];
@@ -88,74 +88,74 @@ function setbkg(){
         var w = moment().tz('America/New_York').get('day');
         img = dayImg[w]; // like this
     }
-    
+
     if (!img) img = 'i.imgur.com/wNOr5VU.png';
-    
+
     var fakeImg = new Image();
-    fakeImg.onload = function() {
+    fakeImg.onload = function () {
         // getting the size of the window
         var winsize = getWindowSize();
         var win_w = winsize[0];
         var win_h = winsize[1];
-        
+
         // getting the ratio of the original image
-        var ratio_wh = this.width/this.height;
-        var ratio_hw = this.height/this.width;
-        
+        var ratio_wh = this.width / this.height;
+        var ratio_hw = this.height / this.width;
+
         // calculating the new size of the img
         var div_w = 0;
         var div_h = 0;
         if (win_w > win_h) {
-            div_h = win_h-5;
-            div_w = div_h*ratio_wh;
+            div_h = win_h - 5;
+            div_w = div_h * ratio_wh;
         } else {
-            div_w = win_w-5;
-            div_h = div_w*ratio_hw;
+            div_w = win_w - 5;
+            div_h = div_w * ratio_hw;
         }
-        
+
         // getting the changing ratio
-        var ratioH = div_h/this.height;
-        
+        var ratioH = div_h / this.height;
+
         paddingTop *= ratioH;
         outerHand *= ratioH;
         fullHandSize *= ratioH;
-        
+
         // updating the size of the container
-        var div = document.getElementById('divContainer');
-        div.style.height = div_h+'px';
-        div.style.width = div_w+'px';
-        
+        var div = document.getElementById('clock');
+        div.style.height = div_h + 'px';
+        div.style.width = div_w + 'px';
+
         // updating the size of the image
-        var imgBg = document.getElementById('myBkg');
-        imgBg.style.height = div_h+'px';
-        imgBg.style.width = div_w+'px';
-        
+        var imgBg = document.getElementById('clockface');
+        imgBg.style.height = div_h + 'px';
+        imgBg.style.width = div_w + 'px';
+
         // updating the position of the hand
         var hand = document.getElementById('minuteHand');
         // centering the hand horizontally
-        var left = div_w/2;
-        hand.style.left = (left+2)+'px';
-        
+        var left = div_w / 2;
+        hand.style.left = (left + 2) + 'px';
+
         // adjusting the distance from the top
-        hand.style.top = Math.round(paddingTop - outerHand)+'px';
-        
+        hand.style.top = Math.round(paddingTop - outerHand) + 'px';
+
         // adjusting the height of the hand
-        hand.style.height = Math.round(fullHandSize)+'px';
+        hand.style.height = Math.round(fullHandSize) + 'px';
         hand.style.WebkitTransformOrigin = "center bottom";
         hand.style.MozTransformOrigin = "center bottom";
         hand.style.transformOrigin = "center bottom";
-        
+
         // after the fakeImg is loaded, then we update the real one
         imgBg.src = this.src;
     };
-    
+
     // we need to create a fake image to get its size and adjust everything
-    fakeImg.src = 'http://npr.today/img/'+img;
-    
+    fakeImg.src = 'http://npr.today/img/' + img;
+
     // we'll check again every 5 seconds
     clearTimeout(waitForIt);
-    waitForIt = setTimeout(setbkg,1000*5);
-    
+    waitForIt = setTimeout(setbkg, 1000 * 5);
+
     // updating the minute hand
     updateMinuteHand();
 }
@@ -166,19 +166,19 @@ function updateMinuteHand() {
     var h = moment().tz('America/New_York').get('hour');
     var m = moment().tz('America/New_York').get('minute');
     var s = moment().tz('America/New_York').get('second');
-    
-    var radMinute = 360/60;
-    var radSecond = (radMinute-2)/60; // -2 to account for small lag behind real time in mp3 stream
-    
+
+    var radMinute = 360 / 60;
+    var radSecond = (radMinute - 2) / 60; // -2 to account for small lag behind real time in mp3 stream
+
     // if you do NOT want the hand to move as the seconds go, just set 'radSecond' to zero
-    
-    var radius = radMinute*m + radSecond*s;
-    
+
+    var radius = radMinute * m + radSecond * s;
+
     var hand = document.getElementById('minuteHand');
-    hand.style.WebkitTransform = "rotate("+radius+"deg)";
-    hand.style.MozTransform = "rotate("+radius+"deg)";
-    hand.style.transformOrigin = "rotate("+radius+"deg)";
-    
+    hand.style.WebkitTransform = "rotate(" + radius + "deg)";
+    hand.style.MozTransform = "rotate(" + radius + "deg)";
+    hand.style.transformOrigin = "rotate(" + radius + "deg)";
+
     clearTimeout(delayMinuteHand);
-    delayMinuteHand = setTimeout(updateMinuteHand,1000);
+    delayMinuteHand = setTimeout(updateMinuteHand, 1000);
 }
--- moment-timezone-with-data.js
+++ moment-timezone-with-data.js
@@ -1,5 +1,5 @@
 //! moment-timezone.js
-//! version : 0.5.11
+//! version : 0.5.13
 //! Copyright (c) JS Foundation and other contributors
 //! license : MIT
 //! github.com/moment/moment-timezone
@@ -24,7 +24,7 @@
 	// 	return moment;
 	// }
 
-	var VERSION = "0.5.11",
+	var VERSION = "0.5.13",
 		zones = {},
 		links = {},
 		names = {},
@@ -596,9 +596,9 @@
 	}
 
 	loadData({
-		"version": "2016j",
+		"version": "2017b",
 		"zones": [
-			"America/New_York|EST EDT EWT EPT|50 40 40 40|01010101010101010101010101010101010101010101010102301010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010|-261t0 1nX0 11B0 1nX0 11B0 1qL0 1a10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 RB0 8x40 iv0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1cN0 1cL0 1cN0 1cL0 s10 1Vz0 LB0 1BX0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0|21e6",
+			"America/New_York|EST EDT|50 40|01010101010101010101010|1GI70 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0|21e6"
 		],
 		"links": [
 			"America/New_York|US/Eastern"
@@ -608,4 +608,3 @@
 
 	return moment;
 }));
-0
